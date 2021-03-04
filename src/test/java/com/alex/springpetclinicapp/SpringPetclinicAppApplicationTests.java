9fb964ba987f385c1942b82cc8f65838deb11250
alex3195 <alex9954161@mail.ru>
2021-03-04 08:38:22 +0500

    Initial project

---
+++ src/test/java/com/alex/springpetclinicapp/SpringPetclinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.springpetclinicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
